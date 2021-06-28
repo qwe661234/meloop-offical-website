8c66cfb3ad4424fb63bb328265df34d589b8e407
Yen Fu Chen <qwe661234@gmail.com>
2021-06-28 16:13:44 +0800

    feat: add plain text in privacy, useTerm and advertising
    feat: add plain text in privacy, useTerm and advertising

--- pages/advertising.vue
+++ pages/advertising.vue
@@ -1,7 +1,22 @@
 <template>
-  <div> Advertising </div>
+  <div>
+    <div class="section purple">
+      <div class="label">ＭeLoop廣告投放</div>
+      <div class="text">
+        還在苦惱自己的產品沒有年輕人認識嗎？恭喜你！在 Ｍe Loop
+        內有100%的活耀大學生。現在您可以選擇投放廣告來推廣您的重要產品或活動訊息，讓內容出現在顯眼的版位頁面以及
+        Ｍe Loop 經營的相關粉絲團，進而獲得更多的關注瀏覽與效益喔！此外，在 Ｍe
+        Loop
+        內擁有區域型廣告的特性，可針對想曝光的學校進行篩選，達到精準行銷的效果。想讓品牌受眾年輕化嗎？那就交給
+        Ｍe Loop 吧！
+      </div>
+    </div>
+    <div class="section purple">
+    
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {};
+export default {};
 </script>
