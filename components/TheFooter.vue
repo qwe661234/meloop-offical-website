f9678912043ed94b7ee7ea04ca8b21b959d754a8
Yen Fu Chen <qwe661234@gmail.com>
2021-06-28 15:29:22 +0800

    fix: add icon in footer and header

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,10 +1,9 @@
 <template>
 <div>
-    <a href="https://www.facebook.com/i.loop.u/"> FB </a>
-    <a href="https://www.instagram.com/i.loop.u/"> IG </a>
+    <a href="https://www.facebook.com/i.loop.u/"><img src="/images/fbIcon.png"/></a>
+    <a href="https://www.instagram.com/i.loop.u/"><img src="/images/igIcon.png"/></a>
     <nuxt-link to="/useTerm"> 使用條款 </nuxt-link>
     <nuxt-link to="/privacy"> 隱私政策 </nuxt-link>
-    
 </div>
 </template>
 
--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <nuxt-link to="/"> Home </nuxt-link>
+    <nuxt-link to="/"><img src="/images/logo.png" alt="logo" /></nuxt-link>
     <nuxt-link to="/"> 首頁 </nuxt-link>
     <nuxt-link to="/Advertising"> 廣告投放 </nuxt-link>
-    <a href="https://www.facebook.com/i.loop.u/"> FB </a>
-    <a href="https://www.instagram.com/i.loop.u/"> IG </a>
+    <a href="https://www.facebook.com/i.loop.u/"><img src="/images/fbIcon.png" alt="fbIcon" /></a>
+    <a href="https://www.instagram.com/i.loop.u/"><img src="/images/igIcon.png" alt="igIcon" /></a>
   </div>
 </template>
 
