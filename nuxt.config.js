873885558110a88bac6abdc710a4c504f86319f6
Yen Fu Chen <qwe661234@gmail.com>
2021-07-02 17:23:29 +0800

    fix: modify router

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,7 @@ export default {
     // Modules: https://go.nuxtjs.dev/config-modules
     modules: ["@nuxtjs/style-resources"],
 
-    route: {
+    router: {
         base: "/meloop-offical-website/",
     },
     // Build Configuration: https://go.nuxtjs.dev/config-build
