b12ba44b85ee70f5275ee3090c364a50b4841c3c
qwe661234 <qwe661234@gmail.com>
2021-10-03 22:27:26 +0800

    33571e4

---
+++ advertise/adv.js
@@ -0,0 +1,26 @@
+export const ADVERTISES = [
+    {
+        title: "MeLoop粉絲頁曝光",
+        text: "Ｍe Loop 經營的粉絲團，封面圖片、客製圖文皆可進行合作曝光。",
+        img: require("~/assets/images/advertise1.jpeg"),
+        price: "一則500元",
+    },
+    {
+        title: "MeLoop APP首頁橫幅廣告",
+        text: "一次可購買最多30天，需自行提供合適尺寸的banner圖片(686*320)",
+        img: require("~/assets/images/advertise2.jpeg"),
+        price: "每日5000元",
+    },
+];
+
+export const ADVERTISETEXT = {
+    label: "ＭeLoop廣告投放",
+    plainText: `
+    還在苦惱自己的產品沒有年輕人認識嗎？恭喜你！在 Ｍe Loop
+    內有100%的活耀大學生。現在您可以選擇投放廣告來推廣您的重要產品或活動訊息，讓內容出現在顯眼的版位頁面以及
+    Ｍe Loop 經營的相關粉絲團，進而獲得更多的關注瀏覽與效益喔！此外，在 Ｍe
+    Loop
+    內擁有區域型廣告的特性，可針對想曝光的學校進行篩選，達到精準行銷的效果。想讓品牌受眾年輕化嗎？那就交給
+    Ｍe Loop 吧！
+    `,
+};
---
+++ link/link.js
@@ -0,0 +1,6 @@
+export const appleStoreLink =
+    "https://apps.apple.com/tw/app/me-loop-%E6%8E%A2%E7%B4%A2%E7%94%9F%E6%B4%BB%E3%81%AE%E6%AF%8F%E4%B8%80%E6%AD%A5/id1532001343";
+export const googlePlayLink =
+    "https://play.google.com/store/apps/details?id=fun.meloop.app";
+export const fbLink = "https://www.facebook.com/i.loop.u/";
+export const igLink = "https://www.instagram.com/i.loop.u/";
